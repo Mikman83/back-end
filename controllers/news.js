0c7f29a6bc9d0e10f998ab0c31dadb800f653ce6
Mika <ivlev.michael@gmail.com>
2022-05-14 13:26:08 +0300

    add news request

--- controllers/news.js
+++ controllers/news.js
@@ -1,5 +1,22 @@
-module.exports.getNews = function (request, response) {
-  response.status(200).json({
-    news: 'getNews from controller',
-  });
+const News = require('../models/News');
+const errorHandler = require('../utils/error-handlers');
+
+module.exports.getNews = async function (request, response) {
+  try {
+    const news = await News.find();
+    response.status(200).json(news);
+  } catch(e) {
+    errorHandler(response, e);
+  }
+};
+
+module.exports.addNews = async function (request, response) {
+  try {
+    const news = await new News({
+      news: request.body.news
+    }).save();
+    response.status(201).json(news);
+  } catch(e) {
+    errorHandler(response, e);
+  }
 };
--- routes/news.js
+++ routes/news.js
@@ -6,5 +6,6 @@ const controller = require('../controllers/news');
 const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), controller.getNews);
+router.post('/', passport.authenticate('jwt', { session: false }), controller.addNews);
 
 module.exports = router;
